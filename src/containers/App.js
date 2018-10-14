01b353f08c0e75212baf4ee82a795a42f6dd1c0f
OriginUnknown <ugoego@gmail.com>
2018-10-14 16:13:26 +0100

    [react-app] - Unsaved files from previous commit Task: Work done:

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import APP_CONST from '../resources/en-GB';
-<<<<<<< HEAD
 import Header from '../components/Navigation/Header';
-=======
->>>>>>> 78c021e0639712744e3e5e9d49c99d143864f03e
 
 import './App.css';
 
@@ -11,10 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
       <Header title={APP_CONST.NAVIGATION.HEADER.title } />
-=======
->>>>>>> 78c021e0639712744e3e5e9d49c99d143864f03e
           <p>{ APP_CONST.TEST.MESSAGE }</p>
       </div>
     );
--- src/resources/en-GB.js
+++ src/resources/en-GB.js
@@ -1,12 +1,9 @@
 const APP_CONST = {
-<<<<<<< HEAD
     NAVIGATION: {
         HEADER: {
             title: "My Notes..."
         }
     },
-=======
->>>>>>> 78c021e0639712744e3e5e9d49c99d143864f03e
     TEST: {
         MESSAGE: "Yey! I'm using react!!"
     }
